1a584995e0749f63ebbd8adfb902412f7ecebe44
Hong Ngoc Doan Thi <mailtocuong@gmail.com>
2020-08-17 11:36:40 +0300

    put all file in node folder

--- node/app.js
+++ node/app.js
@@ -4,6 +4,9 @@ const express = require("express");
 
 const app = express();
 
+// allow to add middle which will be execute in every request
+app.use((req, res, next) => {});
+
 const server = http.createServer(app);
 
 server.listen(3000);
