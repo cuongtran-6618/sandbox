38ce1e8c6ac7b0737602a3d7e9fb13b84ff916ca
Hong Ngoc Doan Thi <mailtocuong@gmail.com>
2020-08-17 16:11:37 +0300

    use express, clean code

--- node/app.js
+++ node/app.js
@@ -1,12 +1,10 @@
-const http = require("http");
-
 const express = require("express");
 
 const app = express();
 
 // allow to add middle which will be execute in every request
-app.use((req, res, next) => {});
-
-const server = http.createServer(app);
+app.use((req, res, next) => {
+	res.send();
+});
 
 server.listen(3000);
